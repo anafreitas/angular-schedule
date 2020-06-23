5f3a7a1aeaf40234988a4cbab3d35b645a012418
Ana Claudia Carvalho <ana.carvalho@sankhyacode.com>
2020-06-23 01:11:31 -0300

    stars

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,15 +3,18 @@ import { CommonModule } from '@angular/common';
 import { ProfessionalCardComponent } from './professional-card/professional-card.component';
 import { MaterialComponentsModule } from './material-components.module';
 import { RouterModule } from '@angular/router';
+import { NgbRating } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
     declarations: [
-        ProfessionalCardComponent
+        ProfessionalCardComponent,
+        NgbRating
     ],
     imports: [
         CommonModule,
         RouterModule,
         MaterialComponentsModule
+
     ],
     exports: [
         ProfessionalCardComponent
